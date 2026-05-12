4358c80e01cbec5c1f3f4b6cf70ae472840f65b8
Abneer03 <abneertof03@outlook.com>
2026-05-11 20:52:23 -0600

    Bloque 1 - Paso 3: funciones

---
+++ funciones.py
@@ -0,0 +1,8 @@
+def calcular_area (b, h):
+    return (b * h) 
+
+print ("Calculemos el area de cualquier rectángulo")
+base = int(input ("Primero dame el largo del rectángulo"))
+altura = int(input("Ahora ingresa el ancho del rectángulo"))
+resultado = calcular_area (base, altura) 
+print ("El area del rectángulo es" ,resultado, "gracias por confiar en mi")
