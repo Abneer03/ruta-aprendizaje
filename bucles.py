5aaf83305f3754238c59c1331bf8f25681c39c07
Abneer03 <abneertof03@outlook.com>
2026-05-11 19:46:40 -0600

    Bloque 1: - Paso 2: bucles for y while

---
+++ bucles.py
@@ -0,0 +1,17 @@
+# Bucle for
+for i in range(5):
+    print(i)
+
+# Bucle for con lista
+frutas = ["manzana", "pera", "uva", "mango"]
+
+for fruta in frutas:
+    print(fruta)
+
+# Bucle while
+contador = 0
+
+while contador < 5:
+    print(contador)
+    contador = contador + 1
+    
